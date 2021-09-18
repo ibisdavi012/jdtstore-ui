762a1d3343c63dbce14388a6b4f83b0a7ea86f05
Ibis David Márquez Bouttó <ibisdavi012@gmail.com>
2021-09-18 01:48:51 -0400

    Added State Management to Add-Product-Form

--- src/components/AddProductForm.js
+++ src/components/AddProductForm.js
@@ -4,7 +4,11 @@ import { useSelector } from "react-redux";
 import "../sass/components/add-product-form.scss";
 
 export default function AddProductForm() {
+  const [productSku, setProductSku] = useState("");
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState(0);
   const [productType, setProductType] = useState("Dvd");
+  const [productSpecifics, setProductSpecifics] = useState({});
 
   const productTypeChanged = (e) => {
     setProductType(e.target.value || "Dvd");
@@ -15,30 +19,73 @@ export default function AddProductForm() {
   useEffect(() => {
     setTimeout(() => {
       [...document.getElementsByClassName("dynamic-field")].forEach(
-        (hiddenField) => {
-          hiddenField.style.display = hiddenField.classList.contains("hidden")
-            ? "none"
-            : "block";
+        (dynamicField) => {
+          dynamicField.style.display = dynamicField.classList.contains(
+            "visible"
+          )
+            ? "block"
+            : "none";
         }
       );
     }, 500);
-  }, [productType]);
+
+    switch (productType) {
+      case "Dvd":
+        setProductSpecifics({ size: 0 });
+        break;
+      case "Book":
+        setProductSpecifics({ weight: 0 });
+        break;
+      case "Furniture":
+        setProductSpecifics({ height: 0, width: 0, length: 0 });
+        break;
+      default:
+        setProductSpecifics({});
+    }
+
+    if (formStatus === "SAVED") {
+      alert("Yes!");
+    }
+  }, [productType, formStatus]);
 
   return (
     <form id="product_form" autoComplete="off">
+      <h1>{formStatus}</h1>
       <div className="form-group">
         <label>SKU</label>
-        <input id="sku" type="text" placeholder="product's SKU" />
+        <input
+          id="sku"
+          type="text"
+          placeholder="product's SKU"
+          onChange={(e) => setProductSku(e.target.value)}
+          value={productSku}
+        />
       </div>
 
       <div className="form-group">
         <label>Name</label>
-        <input id="name" type="text" placeholder="product's name" />
+        <input
+          id="name"
+          type="text"
+          placeholder="product's name"
+          onChange={(e) => {
+            setProductName(e.target.value);
+          }}
+          value={productName}
+        />
       </div>
 
       <div className="form-group">
         <label>Price($)</label>
-        <input id="price" type="text" placeholder="product's price" />
+        <input
+          id="price"
+          type="text"
+          placeholder="product's price"
+          onChange={(e) => {
+            setProductPrice(e.target.value);
+          }}
+          value={productPrice}
+        />
       </div>
 
       <div className="form-group">
@@ -67,7 +114,15 @@ export default function AddProductForm() {
         }`}
       >
         <label>Size (Mb)</label>
-        <input id="size" type="text" placeholder="dvd's size" />
+        <input
+          id="size"
+          type="text"
+          placeholder="dvd's size"
+          onChange={(e) => {
+            setProductSpecifics({ size: e.target.value });
+          }}
+          value={productSpecifics.size || ""}
+        />
       </div>
       <div
         className={`form-group dynamic-field ${
@@ -75,7 +130,15 @@ export default function AddProductForm() {
         }`}
       >
         <label>Weight (Kg)</label>
-        <input id="weight" type="text" placeholder="books's weight" />
+        <input
+          id="weight"
+          type="text"
+          placeholder="books's weight"
+          onChange={(e) => {
+            setProductSpecifics({ weight: e.target.value });
+          }}
+          value={productSpecifics.weight || ""}
+        />
       </div>
       <div
         className={`dynamic-field ${
@@ -84,15 +147,48 @@ export default function AddProductForm() {
       >
         <div className="form-group">
           <label>Height (cm)</label>
-          <input id="height" type="text" placeholder="furniture's height" />
+          <input
+            id="height"
+            type="text"
+            placeholder="furniture's height"
+            onChange={(e) => {
+              setProductSpecifics({
+                ...productSpecifics,
+                height: e.target.value,
+              });
+            }}
+            value={productSpecifics.height || ""}
+          />
         </div>
         <div className="form-group">
           <label>Width (cm)</label>
-          <input id="width" type="text" placeholder="furniture's width" />
+          <input
+            id="width"
+            type="text"
+            placeholder="furniture's width"
+            onChange={(e) => {
+              setProductSpecifics({
+                ...productSpecifics,
+                width: e.target.value,
+              });
+            }}
+            value={productSpecifics.width || ""}
+          />
         </div>
         <div className="form-group">
           <label>Length($)</label>
-          <input id="length" type="text" placeholder="furniture's length" />
+          <input
+            id="length"
+            type="text"
+            placeholder="furniture's length"
+            onChange={(e) => {
+              setProductSpecifics({
+                ...productSpecifics,
+                length: e.target.value,
+              });
+            }}
+            value={productSpecifics.length || ""}
+          />
         </div>
       </div>
     </form>
