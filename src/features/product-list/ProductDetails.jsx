11fb9a8a04893e2f0bc4908bed44fa67a0264a33
Ibis David Márquez Bouttó <ibisdavi012@gmail.com>
2021-09-30 13:49:50 -0400

    Added SKU to the products details and removed id

--- src/features/product-list/ProductDetails.jsx
+++ src/features/product-list/ProductDetails.jsx
@@ -4,7 +4,7 @@ import ProductSpecifics from "./ProductSpecifics";
 export default function ProductDetails({ product }) {
   return (
     <div className="product__details">
-      <div className="product__id">{product.id}</div>
+      <div className="product__sku">{product.sku}</div>
       <div className="product__description">{product.name}</div>
       <div className="product__price">$ {product.price}</div>
       <div className="product__info">
