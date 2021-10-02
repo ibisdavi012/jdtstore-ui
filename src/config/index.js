288f08974a5646ae75689b44924f7f348c66fe21
Ibis David Márquez Bouttó <ibisdavi012@gmail.com>
2021-10-02 00:37:03 -0400

    UI Tweaks

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ export const config = {
   loader: false,
   useFetch: true,
   endpoints: {
-    products: 'https://jdtstore.000webhostapp.com/products'
+    products: 'http://localhost/products'
   },
 };
