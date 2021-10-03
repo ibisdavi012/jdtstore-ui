a28f8881786535d9741686d0b50febf145dd4785
Ibis David Márquez Bouttó <ibisdavi012@gmail.com>
2021-10-03 15:37:21 -0400

    Display price in American currency format

--- src/features/product-list/ProductDetails.jsx
+++ src/features/product-list/ProductDetails.jsx
@@ -2,11 +2,12 @@ import "./product-details.scss";
 import ProductSpecifics from "./ProductSpecifics";
 
 export default function ProductDetails({ product }) {
+  
   return (
     <div className="product__details">
       <div className="product__sku">{product.sku}</div>
       <div className="product__description">{product.name}</div>
-      <div className="product__price">$ {product.price}</div>
+      <div className="product__price">$ {Intl.NumberFormat('en-US').format(product.price)}</div>
       <div className="product__info">
         <ProductSpecifics product={product} />
       </div>
